7ccce0fc3a2970796afe57e2cb3db42066813721
Warun Kumar <buntykmr2@gmail.com>
2025-01-25 17:08:59 +0530

    add file read with dvc

--- src/tune_logreg.py
+++ src/tune_logreg.py
@@ -2,7 +2,6 @@ import mlflow
 import mlflow.sklearn
 import pandas as pd
 
-from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
@@ -15,12 +14,25 @@ from sklearn.metrics import (
 )
 
 
-def setup_data() :
-    # 1. Load the Iris dataset as a DataFrame for convenience
-    data = load_iris()
+def read_data_from_file(file_path="data/iris_data.csv"):
+    """
+    Reads the Iris dataset from a file and returns X_df and y_df.
+    """
+    # Read the CSV file
+    iris_df = pd.read_csv(file_path)
     
-    X_df = pd.DataFrame(data.data, columns=data.feature_names)
-    y_df = pd.Series(data.target, name="target")
+    # Separate features (X_df) and target (y_df)
+    X_df = iris_df.drop(columns=["target"])
+    y_df = iris_df["target"]
+    return X_df, y_df
+
+def setup_data():
+    """
+    Reads the Iris dataset from a file, splits it into train/test sets,
+    and returns the split data.
+    """
+    # Read the dataset from the file
+    X_df, y_df = read_data_from_file()
 
     # 2. Split into train/test
     return train_test_split(
